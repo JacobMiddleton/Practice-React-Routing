421bcfb1139482ba61d6368aa6e844da5efea817
Jacob Middleton <109218193+JacobMiddleton@users.noreply.github.com>
2022-12-22 11:23:06 -0500

    Import Route

--- react-router-lesson-starter/src/app/App.js
+++ react-router-lesson-starter/src/app/App.js
@@ -9,7 +9,7 @@ import Categories from "../components/Categories";
 import Author from "../components/Author";
 import Profile from "../components/Profile";
 
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import "./App.css";
 
