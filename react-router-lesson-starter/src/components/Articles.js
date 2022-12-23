0354c14d84752bcaf3084e6daae903ecbdb78ffa
Jacob Middleton <109218193+JacobMiddleton@users.noreply.github.com>
2022-12-23 12:33:12 -0500

    Implement search function

--- react-router-lesson-starter/src/components/Articles.js
+++ react-router-lesson-starter/src/components/Articles.js
@@ -3,28 +3,28 @@ import { useSelector } from "react-redux";
 import { selectArticles, filterArticles } from "../features/articles/articlesSlice";
 import Search from "./Search";
 
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-export default function Articles () {
+export default function Articles() {
   const articles = useSelector(selectArticles)
-  
-  // grab the search value from useLocation()
-  
-  // get the queryParams from new URLSearchParams() 
-  
-  const title = '' // <-- fix me!
-  
+
+  const { search } = useLocation();
+  console.log(search);
+
+  const queryParams = new URLSearchParams(search);
+
+  const title = queryParams.get('title');
+
   const filteredArticles = title ? filterArticles(title, articles) : Object.values(articles)
 
   return (
     <main>
       <h1>Articles</h1>
       <ul>
-        { 
+        {
           filteredArticles.map(article => {
             return (
               <li key={article.slug}>
-                {/* Replace these a tags! */}
                 <Link to={`/articles/${article.slug}`}>
                   {article.title}
                 </Link>
